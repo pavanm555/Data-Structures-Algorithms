bf26d593536c0413336c548fd926fd767c7eb47a
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-07-24 22:04:45 +0530

    Add files via upload

--- 06_Vectors/03_vector_class_1.cpp
+++ 06_Vectors/03_vector_class_1.cpp
@@ -4,21 +4,23 @@ using namespace std;
 class Vector{
     // Data Members
     int *arr;
-    int cs;
-    int ms;
+    int cs; // Current size
+    int ms; // Max size
 public:
     // Constructor, Destructor, Methods
     Vector(){
-        cs = 0;
+        cs = 0; 
         ms = 1;
         arr = new int[ms];
     }
 
     void push_back(int d){
-        //Two cases
+        // Two cases
         if(cs==ms){
-            // Create a new array and delete the old one
+            // Create a new array and delete the old one, double the capacity
             int *oldArr = arr;
+            ms = 2*ms;
+            arr = new int[ms];
             // Copy the elements
             for(int i=0;i<cs;i++){
                 arr[i] = oldArr[i];
@@ -31,10 +33,14 @@ public:
     }
 
     void pop_back(){
-        
+        if(cs>=0){
+            cs--; 
+        }      
     }
-    
 
+    bool isEmpty(){
+        return cs==0;
+    }
 };
 
 int main(){
