c3c5392f74e99f681313d183a58dbae3fa0d9012
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-07-26 14:19:02 +0530

    Add files via upload

--- 06_Vectors/03_vector_class_1.cpp
+++ 06_Vectors/03_vector_class_1.cpp
@@ -8,13 +8,13 @@ class Vector{
     int ms; // Max size
 public:
     // Constructor, Destructor, Methods
-    Vector(){
+    Vector(int max_size=1){
         cs = 0; 
-        ms = 1;
+        ms = max_size;
         arr = new int[ms];
     }
 
-    void push_back(int d){
+    void push_back(const int d){
         // Two cases
         if(cs==ms){
             // Create a new array and delete the old one, double the capacity
@@ -38,11 +38,55 @@ public:
         }      
     }
 
-    bool isEmpty(){
+    bool isEmpty() const{
         return cs==0;
+    }
+
+    // Front, Back, At(i), []
+    int front() const{
+        return arr[0];
+    }
+
+    int back() const{
+        return arr[cs-1];
+    }
+    
+    int at(int i) const{
+        return arr[i];
+    }
+    
+    int size() const{
+        return cs;
+    }
+    
+    int capacity() const{
+        return ms;
+    }
+
+    int operator[](const int i) const{
+        return arr[i];
     }
 };
 
 int main(){
     Vector v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(15);
+    v.push_back(6);
+    v.pop_back();
+    cout<< v.front() <<endl;
+    cout<< v.back() <<endl;
+
+    cout<< v.at(2) <<endl;
+
+    cout<< v.size()<< endl;
+    cout<< v.capacity()<< endl;
+
+    for(int i=0;i<v.size();i++){
+        cout<< v[i] << ", "; 
+    }
 }
