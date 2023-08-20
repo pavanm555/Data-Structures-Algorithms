0fb5a34d8241affbbad095449a8ef5b67c85084f
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-08-20 11:50:44 +0530

    Add files via upload

---
+++ 16_Binary_Tree/01_binary_tree.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node * left;
+    Node * right;
+    Node(int d){
+        data = d ;
+        left = right = NULL;
+    }
+};
+
+int main(){
+
+    return 0;
+}
