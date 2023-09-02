dc840b708cc9c8800ea9200e15e09c3831df498f
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-09-02 19:47:15 +0530

    Add files via upload

---
+++ 18_Heaps_Priority_Queue/01_heap.cpp
@@ -0,0 +1,12 @@
+// 1.Binary Tree
+// 2.Complete Binary Tree - filling of the tree must be in the left to right order
+// 3.Heap Order Property - Max heap (parent >= children)
+
+/*
+
+ Heaps as an Array
+    i
+   / \
+ 2i   2i+1
+
+ */
