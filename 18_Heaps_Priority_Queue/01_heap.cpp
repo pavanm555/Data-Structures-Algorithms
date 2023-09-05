71ffd2bc23cb09d252efd68dbc5625b728182456
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-09-05 22:18:32 +0530

    Add files via upload

--- 18_Heaps_Priority_Queue/01_heap.cpp
+++ 18_Heaps_Priority_Queue/01_heap.cpp
@@ -1,6 +1,7 @@
 // 1.Binary Tree
 // 2.Complete Binary Tree - filling of the tree must be in the left to right order
 // 3.Heap Order Property - Max heap (parent >= children)
+//                         Min heap (parent <= children)
 
 /*
 
@@ -10,3 +11,23 @@
  2i   2i+1
 
  */
+
+#include<iostream>
+#include "heap.h"
+using namespace std;
+
+int main(){
+    Heap h;
+    h.push(10);
+    h.push(20);
+    h.push(30);
+    h.push(25);
+    h.push(70);
+    h.push(40);
+    h.push(50);
+    
+    cout<< h.top();
+    
+
+  return 0;
+}
