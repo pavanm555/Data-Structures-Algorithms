bb7249bbed49fac46f97506b55bfbe8de43fb97d
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-09-22 10:38:02 +0530

    Add files via upload

--- 18_Heaps_Priority_Queue/03_nearest_cars.cpp
+++ 18_Heaps_Priority_Queue/03_nearest_cars.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
 
@@ -12,21 +13,53 @@ public:
         this->x = x;
         this->y = y;
     }
-    int dist(){
+    int dist() const{
         return x*x + y*y; //square of the dist
     }
 };
 
 class CarCompare{
+public:
     bool operator()(const Car A, const Car B){
         return A.dist() < B.dist(); // max heap
     }
 };
 
+bool distCompare(Car A,Car B){
+    return A.dist() < B.dist(); //ascending order
+}
+
 void printNearestCars(vector<Car> cars,int k){
 
     //create a max heap of size K
-    priority_queue<Car, vector<Car>, CarCompare max_heap(cars.begin(), cars.begin()+k);
+    priority_queue<Car, vector<Car>, CarCompare> max_heap(cars.begin(), cars.begin() + k);
+
+    //remaining cars
+    for(int i=k; i<cars.size();i++){
+        auto car = cars[i];
+
+        if(car.dist() < max_heap.top().dist()){
+            max_heap.pop(); //remove the root node largest
+            max_heap.push(car);
+        }
+    }
+
+    //print all the cars inside the heap they will be smallest
+    vector<Car> output;
+    while(!max_heap.empty()){
+        // cout<< max_heap.top().id <<endl;
+        output.push_back(max_heap.top());
+        max_heap.pop();
+    }
+
+    //vector of k nearest cars
+    sort(output.begin(), output.end(), distCompare);
+
+    for(auto car:output){
+        cout<< car.id <<endl;
+    }
+
+    return;
 
 }
 
