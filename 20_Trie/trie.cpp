852d186dee52c1f58a970846ef67069b917ce303
Pavan M <110841565+pavanm555@users.noreply.github.com>
2023-09-28 00:07:38 +0530

    Add files via upload

---
+++ 20_Trie/trie.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "trie.h"
+using namespace std;
+
+int main(){
+    string words[] = {"hello","he","apple","aple","news"};
+    Trie t;
+    for(auto word:words){
+        t.insert(word);
+    }
+
+    string key;
+    cin>>key;
+    cout<< t.search(key) <<endl;
+}
